1606e9f1284919af3716014aaf64da9d11b041df
Ashkan Abbasi <airashkan@yahoo.com>
2020-05-27 16:50:27 -0600

    Create dashboard and base controllers, and create merchant dashboard.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "merchants#index"
+  namespace :merchant do
+    get '/', to: 'dashboard#index'
+  end
 
   get "/register", to: 'users#new'
   post "/users", to: 'users#create'
---
+++ spec/features/users/user_merchant_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe "As a merchant" do
+  it "can log in with as a merchant" do
+    user = create(:user, role: 1)
+    visit "/"
+    click_on "Login"
+    expect(current_path).to eq('/login')
+    fill_in :email, with: user.email
+    fill_in :password, with: user.password
+    click_on "Log In"
+    expect(current_path).to eq('/profile')
+
+    expect(page).to have_content("Logged in as #{user.name}")
+    expect(page).to have_link("Log out")
+    expect(page).to_not have_link("Register")
+    expect(page).to_not have_link("Login")
+    click_on "Merchant Dashboard"
+    expect(current_path).to eq("/merchant")
+  end
+end

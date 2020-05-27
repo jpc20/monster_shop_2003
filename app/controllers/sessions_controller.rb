4efa9b994ffc077d7a0ebfd2e8f74066d8dfa161
Ashkan Abbasi <airashkan@yahoo.com>
2020-05-27 14:24:08 -0600

    'Make sessions spec and make login and logout function in nav bar.'

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,5 +3,16 @@ class SessionsController<ApplicationController
 
   end
 
+  def create
+    user = User.find_by(email: params[:email])
+    if user.authenticate(params[:password])
+      session[:user_id] = user.id
+      flash[:success] = "Welcome, #{user.name}!"
+      redirect_to '/profile'
+    else
+      flash[:error] = "Sorry, your credentials are bad."
+      render :new
+    end
+  end
 
 end

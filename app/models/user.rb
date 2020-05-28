838bb248482ac1d86f7995b8d02a2659f851cd20
Max Mitrani <mjmitrani168@gmail.com>
2020-05-28 16:25:07 -0600

    add user profile show HTML

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-  validates_presence_of :name, :address, :city, :state, :zip, :password
+  validates_presence_of :name, :address, :city, :state, :zip
+  validates_presence_of :password, on: :create
   validates :email, uniqueness: true, presence: true
   has_secure_password
   enum role: %w(default merchant admin)

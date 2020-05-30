161fed866e11dba175585d8215fdb82a4adac928
Jack Cullen <jpcullen20@gmail.com>
2020-05-30 09:47:54 -0600

    Add spec for admin disabling a merchant

--- spec/features/merchants/index_spec.rb
+++ spec/features/merchants/index_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe 'merchant index page', type: :feature do
     before :each do
       @bike_shop = Merchant.create(name: "Brian's Bike Shop", address: '123 Bike Rd.', city: 'Richmond', state: 'VA', zip: 80203)
       @dog_shop = Merchant.create(name: "Meg's Dog Shop", address: '123 Dog Rd.', city: 'Hershey', state: 'PA', zip: 80203)
+      @admin = create(:user, role:2)
     end
 
     it 'I can see a list of merchants in the system' do
@@ -23,5 +24,22 @@ RSpec.describe 'merchant index page', type: :feature do
 
       expect(current_path).to eq("/merchants/new")
     end
+
+    it "Admin can disabkle a merchant account" do
+      visit "/"
+      click_on "Login"
+      fill_in :email, with: @admin.email
+      fill_in :password, with: @admin.password
+      click_on "Log In"
+      visit "/admin/merchants"
+      click_on ".disable-merchant-#{@bike_shop.id}"
+      expect(page).to have_content("#{@bike_shop.name} has been disabled")
+    end
   end
 end
+
+# When I visit the admin's merchant index page ('/admin/merchants')
+# I see a "disable" button next to any merchants who are not yet disabled
+# When I click on the "disable" button
+# I am returned to the admin's merchant index page where I see that the merchant's account is now disabled
+# And I see a flash message that the merchant's account is now disabled

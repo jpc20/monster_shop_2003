8156e6442251d48784cd9bcf28a8a40419bde3e8
Jane Greene <janegreenecasa@gmail.com>
2020-05-30 11:41:53 -0600

    added link to profile to view my orders completed story 27

--- spec/features/users/user_profile_show_spec.rb
+++ spec/features/users/user_profile_show_spec.rb
@@ -170,5 +170,31 @@ RSpec.describe "User Profile page" do
     expect(page).to have_content("Your password has been changed!")
 
   end
+  it "displays orders link" do
+    @user = create(:user)
+    bike_shop = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
+    tire = bike_shop.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
+    order1 = Order.create(name: @user.name, address: @user.address, city: @user.city,
+      state: @user.state, zip: @user.zip, user_id: @user.id, status: "pending")
+      ItemOrder.create(order_id: order1.id, item_id: tire.id, price: 100, quantity: 1)
+    visit '/'
+    click_on "Login"
+    fill_in :email, with: @user.email
+    fill_in :password, with: @user.password
+    click_on "Log In"
 
+    visit "/profile"
+
+    click_on "My Orders"
+
+    expect(current_path).to eq('/profile/orders')
+  end
 end
+
+# User Story 27, User Profile displays Orders link
+#
+# As a registered user
+# When I visit my Profile page
+# And I have orders placed in the system
+# Then I see a link on my profile page called "My Orders"
+# When I click this link my URI path is "/profile/orders"

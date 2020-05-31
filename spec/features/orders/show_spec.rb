d4d9462e19b74d1671a339d3d36814cf8a7a75d6
Jane Greene <janegreenecasa@gmail.com>
2020-05-31 13:19:35 -0600

    added info for items to order show page story 29 complete with the exception of date testing

--- spec/features/orders/show_spec.rb
+++ spec/features/orders/show_spec.rb
@@ -5,11 +5,11 @@ RSpec.describe "Order show page" do
   @user = create(:user)
   bike_shop = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
   tire = bike_shop.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
-  seat = bike_shop.items.create(name: "Seat", description: "So comfy!", price: 10, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 10)
+  seat = bike_shop.items.create(name: "Seat", description: "So comfy!", price: 10, image: "https://images.unsplash.com/photo-1582743779682-351861923531?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60", inventory: 10)
   order1 = Order.create(name: @user.name, address: @user.address, city: @user.city,
     state: @user.state, zip: @user.zip, user_id: @user.id, status: "pending")
-    ItemOrder.create(order_id: order1.id, item_id: tire.id, price: 100, quantity: 2)
-    ItemOrder.create(order_id: order1.id, item_id: seat.id, price: 10, quantity: 1)
+    item_order1 = ItemOrder.create(order_id: order1.id, item_id: tire.id, price: 100, quantity: 2)
+    item_order2 =ItemOrder.create(order_id: order1.id, item_id: seat.id, price: 10, quantity: 1)
   visit '/'
   click_on "Login"
   fill_in :email, with: @user.email
@@ -20,17 +20,29 @@ RSpec.describe "Order show page" do
 
   click_link "#{order1.id}"
   expect(current_path).to eq("/profile/orders/#{order1.id}")
+  save_and_open_page
 
-  within("#order-#{order1.id}") do
-    expect(page).to have_content(order1.id)
-    expect(page).to have_content("Gatorskins")
-    expect(page).to have_content("Seat")
-    expect(page).to have_content(seat.description)
-    expect(page).to have_content(seat.image)
-    expect(page).to have_content(seat.price)
-    expect(page).to have_content(seat.subtotal) #method in item_order not sure if this will work
+
+  expect(page).to have_content("Order number: #{item_order1.order_id}")
+  within("#order_status") do
     expect(page).to have_content("pending")
-    expect(page).to have_content(3)
+  end
+  within("#item-#{item_order1.item_id}") do
+    expect(page).to have_content("Gatorskins")
+    expect(page).not_to have_content("Seat")
+    expect(page).to have_content(tire.description)
+    expect(page).to have_content(tire.price)
+    expect(page).to have_content(item_order1.subtotal)
+    expect(page).to have_content(2)
+    expect(page).to have_css("img[src*='#{tire.image}']")
+  end
+  within("#item-#{item_order2.item_id}") do
+    expect(page).to have_content("Seat")
+    expect(page).not_to have_content("Gatorskins")
+    expect(page).to have_content(seat.description)
+    expect(page).to have_css("img[src*='#{seat.image}']")
+    expect(page).to have_content(seat.price)
+    expect(page).to have_content(item_order2.subtotal) 
   end
   within("#datecreated") do
     expect(page).to have_content("Created:")#need to come back for date

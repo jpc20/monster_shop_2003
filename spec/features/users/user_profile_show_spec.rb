ce78a6a98b1d146ad56d9a1cc800e8c531c84361
Jane Greene <janegreenecasa@gmail.com>
2020-05-30 18:04:28 -0600

    Wrote test for story 28 to see orders index details, edited css expectation on creation test, model test for order quatity

--- spec/features/users/user_profile_show_spec.rb
+++ spec/features/users/user_profile_show_spec.rb
@@ -189,4 +189,42 @@ RSpec.describe "User Profile page" do
 
     expect(current_path).to eq('/profile/orders')
   end
+  it "see every order I've made" do
+    @user = create(:user)
+    bike_shop = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
+    tire = bike_shop.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
+    seat = bike_shop.items.create(name: "Seat", description: "So comfy!", price: 10, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 10)
+    order1 = Order.create(name: @user.name, address: @user.address, city: @user.city,
+      state: @user.state, zip: @user.zip, user_id: @user.id, status: "pending")
+      ItemOrder.create(order_id: order1.id, item_id: tire.id, price: 100, quantity: 2)
+      ItemOrder.create(order_id: order1.id, item_id: seat.id, price: 10, quantity: 1)
+    visit '/'
+    click_on "Login"
+    fill_in :email, with: @user.email
+    fill_in :password, with: @user.password
+    click_on "Log In"
+
+    visit "/profile/orders"
+
+    expect(page).to have_content(order1.id)
+    save_and_open_page
+    expect(page).to have_content("Created:")#need to come back for date
+    expect(page).to have_content("Last Updated:")#need to come back for date
+    expect(page).to have_content("pending")
+    expect(page).to have_content("Quantity")
+    expect(page).to have_content("Grand Total: $210")
+
+  end
 end
+
+# User Story 28, User Profile displays Orders
+#
+# As a registered user
+# When I visit my Profile Orders page, "/profile/orders"
+# I see every order I've made, which includes the following information:
+# - the ID of the order, which is a link to the order show page
+# - the date the order was made
+# - the date the order was last updated
+# - the current status of the order
+# - the total quantity of items in the order
+# - the grand total of all items for that order

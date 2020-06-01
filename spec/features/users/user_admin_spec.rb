51458d0bb0334bef7b73fd6b9f4a4f7c7ab7fa01
Max Mitrani <mjmitrani168@gmail.com>
2020-05-31 23:52:31 -0600

    add Admin index page showing all orders when they were created and user name.

--- spec/features/users/user_admin_spec.rb
+++ spec/features/users/user_admin_spec.rb
@@ -50,11 +50,32 @@ RSpec.describe "As a Admin" do
   end
   it "Doesn't allow me log in more than once" do
     user = create(:user, role: 2)
+    user_2 = create(:user, email: "greatest.com")
+    @meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
+    @brian = Merchant.create(name: "Brian's Dog Shop", address: '125 Doggo St.', city: 'Denver', state: 'CO', zip: 80210)
+
+    @tire = @meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
+    @pull_toy = @brian.items.create(name: "Pull Toy", description: "Great pull toy!", price: 10, image: "http://lovencaretoys.com/image/cache/dog/tug-toy-dog-pull-9010_2-800x800.jpg", inventory: 32)
+
+    @order_1 = Order.create!(name: 'Meg', address: '123 Stang Ave', city: 'Hershey', state: 'PA', zip: 17033, user_id: user_2.id)
+    @order_2 = Order.create!(name: 'Max', address: '321 Stang Ave', city: 'Sershey', state: 'CO', zip: 20033, user_id: user_2.id)
+
+    @order_1.item_orders.create!(item: @tire, price: @tire.price, quantity: 2, status: "fulfilled")
+    @order_2.item_orders.create!(item: @pull_toy, price: @pull_toy.price, quantity: 3, status: "fulfilled")
+
     visit "/"
     click_on "Login"
     fill_in :email, with: user.email
     fill_in :password, with: user.password
     click_on "Log In"
+    visit "/admin"
+    expect(page).to have_content(@order_1.id)
+    expect(page).to have_content(@order_2.id)
+    expect(page).to have_content(@order_1.created_at)
+    expect(page).to have_content(@order_2.created_at)
+    expect(page).to have_content(@order_1.user.name)
+    expect(page).to have_content(@order_2.user.name)
+
   end
 
 

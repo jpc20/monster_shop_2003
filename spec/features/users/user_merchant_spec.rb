253bc990b30b37f8c06e865dea33e04f47725189
Jack Cullen <jpcullen20@gmail.com>
2020-06-02 10:13:56 -0600

    Add spec for merchant items index

--- spec/features/users/user_merchant_spec.rb
+++ spec/features/users/user_merchant_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe "As a merchant" do
 
     @user = create(:user, role: 1, merchant_id: @bike_shop.id)
     @tire = @bike_shop.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
-    @seat = @bike_shop.items.create(name: "Seat", description: "So comfy!", price: 10, image: "https://images.unsplash.com/photo-1582743779682-351861923531?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60", inventory: 10)
+    @seat = @bike_shop.items.create(name: "Seat", description: "So comfy!", price: 10, image: "https://images.unsplash.com/photo-1582743779682-351861923531?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60", inventory: 10, active?: false)
     @order1 = Order.create(name: @user.name, address: @user.address, city: @user.city,
       state: @user.state, zip: @user.zip, user_id: @user.id, status: "pending")
     @order2 = Order.create(name: @user.name, address: @user.address, city: @user.city,
@@ -105,7 +105,40 @@ RSpec.describe "As a merchant" do
     expect(page).to have_link("Your Items")
     click_on "Your Items"
     expect(current_path).to eq("/merchant/items")
+  end
 
+  it "Can see all items and activate/deactivate them" do
+    visit "/"
+    click_on "Login"
+    expect(current_path).to eq('/login')
+    fill_in :email, with: @user.email
+    fill_in :password, with: @user.password
+    click_on "Log In"
+    click_on "Your Items"
+    within "#item-#{@seat.id}" do
+      expect(page).to have_link(@seat.name)
+      expect(page).to have_content(@seat.description)
+      expect(page).to have_content("Price: $#{@seat.price}")
+      expect(page).to have_content("Inactive")
+      expect(page).to have_content("Inventory: #{@seat.inventory}")
+      expect(page).to have_css("img[src*='#{@seat.image}']")
+      expect(page).to have_link("Activate")
+    end
+
+    within "#item-#{@tire.id}" do
+      expect(page).to have_link(@tire.name)
+      expect(page).to have_content(@tire.description)
+      expect(page).to have_content("Price: $#{@tire.price}")
+      expect(page).to have_content("Active")
+      expect(page).to have_content("Inventory: #{@tire.inventory}")
+      expect(page).to have_css("img[src*='#{@tire.image}']")
+      click_link "Deactivate"
+    end
+    expect(current_path).to eq("/merchant/items")
+    expect(page).to have_content("#{@tire.name} is no longer for sale")
+    within "#item-#{@tire.id}" do
+      expect(page).to have_content("Inactive")
+    end
   end
 
 end

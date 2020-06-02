001225fc177ac1fc66301f4e09132724bac6b1f9
Jack Cullen <jpcullen20@gmail.com>
2020-06-02 11:45:00 -0600

    Merchant can create an item from the merchant item index

--- spec/features/users/user_merchant_spec.rb
+++ spec/features/users/user_merchant_spec.rb
@@ -179,5 +179,46 @@ RSpec.describe "As a merchant" do
     expect(page).to have_content("#{@light.name} has been deleted")
     expect(page).to_not have_css("#item-#{@light.id}")
   end
+  it "Can add an item" do
+    visit "/"
+    click_on "Login"
+    expect(current_path).to eq('/login')
+    fill_in :email, with: @user.email
+    fill_in :password, with: @user.password
+    click_on "Log In"
+    click_on "Your Items"
 
+    name = "Chamois Buttr"
+    price = 18
+    description = "No more chaffin'!"
+    image_url = "https://images-na.ssl-images-amazon.com/images/I/51HMpDXItgL._SX569_.jpg"
+    inventory = 25
+
+    click_button "Add Item"
+    expect(current_path).to eq("/merchants/#{@bike_shop.id}/items/new")
+    fill_in :name, with: name
+    fill_in :price, with: price
+    fill_in :description, with: description
+    fill_in :image, with: image_url
+    fill_in :inventory, with: inventory
+
+    click_button "Create Item"
+    expect(current_path).to eq("/merchant/items")
+    expect(page).to have_content("Your item has been saved")
+    new_item = Item.last
+
+    expect(new_item.name).to eq(name)
+    expect(new_item.price).to eq(price)
+    expect(new_item.description).to eq(description)
+    expect(new_item.image).to eq(image_url)
+    expect(new_item.inventory).to eq(inventory)
+    expect(Item.last.active?).to be(true)
+    expect("#item-#{Item.last.id}").to be_present
+    expect(page).to have_content(name)
+    expect(page).to have_content("Price: $#{new_item.price}")
+    expect(page).to have_css("img[src*='#{new_item.image}']")
+    expect(page).to have_content("Active")
+    expect(page).to have_content(new_item.description)
+    expect(page).to have_content("Inventory: #{new_item.inventory}")
+  end
 end

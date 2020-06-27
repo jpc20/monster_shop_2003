89dd75fca6f4c31c0f522f19920b48be3eec58f9
Max Mitrani <mjmitrani168@gmail.com>
2020-06-27 13:22:11 -0600

    Add Admin and Merchant routes from resources change

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,47 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "merchants#index"
-  namespace :merchant do
-    get '/', to: 'dashboard#index'
-    resources :items
-    get '/orders/:id', to: 'orders#show'
-    patch '/item_orders/:id', to: 'item_orders#update'
-  end
+  # namespace :admin do
+  #   get '/', to: 'dashboard#index'
+  #   resources :users, only: [:index, :show]
+  #   resources :merchants, only: [:index, :show, :update] do
+  #     resources :items
+  #   end
+  # end
+  get '/admin', to: 'admin/dashboard#index', as: :admin_dashboard
+  get '/admin/users/:id', to: 'admin/users#show'
+  get '/admin/users', to: 'admin/users#index'
 
-  namespace :admin do
-    get '/', to: 'dashboard#index'
-    resources :users, only: [:index, :show]
-    resources :merchants, only: [:index, :show, :update] do
-      resources :items
-    end
-  end
+  get '/admin/merchants/:merchant_id/items', to: 'admin/items#index'
+  post '/admin/merchants/:merchant_id/items', to: 'admin/items#create', as: :admin_merchant_items
+  get '/admin/merchants/:merchant_id/items/new', to: 'admin/items#new'
+  patch '/admin/merchants/:merchant_id/items/:id/edit', to: 'admin/items#update'
+  get '/admin/merchants/:merchant_id/items/:id/edit', to: 'admin/items#edit', as: :admin_merchant_item
+  patch '/admin/merchants/:merchant_id/items/:id', to: 'admin/items#update'
+  delete '/admin/merchants/:merchant_id/items/:id', to: 'admin/items#destroy'
+
+  get '/admin/merchants/:id', to: 'admin/merchants#show'
+  get '/admin/merchants', to: 'admin/merchants#index'
+  get '/admin/merchants/:id', to: 'admin/merchants#update'
+  patch '/admin/merchants/:id', to: 'admin/merchants#update'
+  # namespace :merchant do
+  #   get '/', to: 'dashboard#index'
+  #   resources :items
+  #   get '/orders/:id', to: 'orders#show'
+  #   patch '/item_orders/:id', to: 'item_orders#update'
+  # end
+  get '/merchant', to: 'merchant/dashboard#index', as: :merchant_dashboard
+  get '/merchant/orders/:id', to: 'merchant/orders#show'
+  patch '/merchant/item_orders/:id', to: 'merchant/item_orders#update'
+  get '/merchant/items', to: 'merchant/items#index'
+  get '/merchant/items/new', to: 'merchant/items#new'
+  post '/merchant/items', to: 'merchant/items#create'
+  get '/merchant/items/:id/edit', to: 'merchant/items#edit', as: :edit_merchant_item
+  patch '/merchant/items/:id', to: 'merchant/items#update', as: :merchant_item
+  put '/merchant/items/:id', to: 'merchant/items#update'
+  delete '/merchant/items/:id', to: 'merchant/items#destroy'
+
+
 
   get "/register", to: 'users#new'
   get "/register/edit", to: 'users#edit'

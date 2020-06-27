678d2cd141948cbc1be94433b8b079e7d881c93e
Max Mitrani <mjmitrani168@gmail.com>
2020-06-27 13:53:07 -0600

    add merchant items resources

--- config/routes.rb
+++ config/routes.rb
@@ -75,11 +75,14 @@ Rails.application.routes.draw do
   # get "/items/:id", to: "items#show"
   # get "/items/:id/edit", to: "items#edit"
   # patch "/items/:id", to: "items#update"
-  resources :items, only: [:index, :show, :edit, :update]
-  get "/merchants/:merchant_id/items", to: "items#index"
-  get "/merchants/:merchant_id/items/new", to: "items#new"
-  post "/merchants/:merchant_id/items", to: "items#create"
-  delete "/items/:id", to: "items#destroy"
+  # get "/merchants/:merchant_id/items", to: "items#index"
+  # get "/merchants/:merchant_id/items/new", to: "items#new"
+  # post "/merchants/:merchant_id/items", to: "items#create"
+  # delete "/items/:id", to: "items#destroy"
+  resources :items, only: [:index, :show, :edit, :update, :destroy]
+  resources :merchants do
+    resources :items, only: [:index, :new, :create]
+  end
 
   get "/items/:item_id/reviews/new", to: "reviews#new"
   post "/items/:item_id/reviews", to: "reviews#create"
